fc11298dd8e94e45b794f1c34f998036b2f383cd
arifmanawer <112596030+arifmanawer@users.noreply.github.com>
2025-03-15 13:14:52 -0400

    feat: navbar added and recipes page added with random recipes

--- frontend/chefit/src/pages/HomePage.tsx
+++ frontend/chefit/src/pages/HomePage.tsx
@@ -1,13 +1,17 @@
+import Navbar from "@/components/Navbar";
 import { Link } from "react-router-dom";
 
 function HomePage() {
   return (
-    <div>
+    <div className="pt-16">
+      <Navbar />
       <h1>Chef it home</h1>
       <nav>
         <ul>
           <li>
-            <Link to="/recipes">Recipes</Link>
+            <Link to="/recipes" className="text-lg font-bold">
+              Recipes
+            </Link>
           </li>
         </ul>
       </nav>
