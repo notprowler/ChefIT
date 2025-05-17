86cefc8cd719d30cfd1ecc3f47cff2ffe112f97d
Arihant Tiwari <arihanttiwari2005@gmail.com>
2025-05-17 03:50:23 -0400

    Revert "E2E debugging"
    This reverts commit fde0a1dd10c2a10cce70a3f90ba7d627759a473d.

--- frontend/cypress/e2e/recipes.cy.ts
+++ frontend/cypress/e2e/recipes.cy.ts
@@ -2,9 +2,7 @@
 
 describe('Recipes Page', () => {
   it('should display exactly 20 recipes', () => {
-    cy.intercept('GET', '**/recipes**').as('getRecipes');
     cy.visit('http://localhost:4173/recipes');
-    cy.wait('@getRecipes'); 
     cy.get('[data-cy=recipe-card]').should('have.length', 20);
   });
 });
