fde0a1dd10c2a10cce70a3f90ba7d627759a473d
Arihant Tiwari <arihanttiwari2005@gmail.com>
2025-05-17 03:45:48 -0400

    E2E debugging
    E2E runs perfectly locally but fails a test on github actions

--- frontend/cypress/e2e/recipes.cy.ts
+++ frontend/cypress/e2e/recipes.cy.ts
@@ -2,7 +2,9 @@
 
 describe('Recipes Page', () => {
   it('should display exactly 20 recipes', () => {
+    cy.intercept('GET', '**/recipes**').as('getRecipes');
     cy.visit('http://localhost:4173/recipes');
+    cy.wait('@getRecipes'); 
     cy.get('[data-cy=recipe-card]').should('have.length', 20);
   });
 });
